be8fae82eff9f9088092dd70e1e9fe76057af771
Adriel  Alexsander <alexander.sousa@smn.com.br>
2024-11-22 13:57:55 -0300

    identando projec

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const startButton = document.querySelector("button");
     const cards = document.querySelectorAll(".card");
-    const iconButton = document.getElementById('icon-botao');
-    const textButton = document.getElementById('texto-botao');
-    const iconTemp = document.getElementById('icon-temporizador');
-    const projectName = document.getElementById('nome-projeto');
-    const projectTimer = document.getElementById('tempo-projeto');
-    const history = document.getElementById('historico');
-    const ancorIconTemporizador = document.getElementById('item-navegacao-temporizador');
-    const ancorIconLogo = document.getElementById('ancor-logo');
-    const temporizador = document.getElementById('cronometro');
-    const tabelaGrid = document.getElementById('tabela-grid');
-    
+    const iconButton = document.getElementById("icon-botao");
+    const textButton = document.getElementById("texto-botao");
+    const iconTemp = document.getElementById("icon-temporizador");
+    const projectName = document.getElementById("nome-projeto");
+    const projectTimer = document.getElementById("tempo-projeto");
+    const history = document.getElementById("historico");
+    const ancorIconTemporizador = document.getElementById("item-navegacao-temporizador");
+    const ancorIconLogo = document.getElementById("ancor-logo");
+    const temporizador = document.getElementById("cronometro");
+    const tabelaGrid = document.getElementById("tabela-grid");
 
     let timerInterval = 0;
     let isRunning = false;
@@ -20,10 +19,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const iniciarOuPausarCronometro = () => {
         const timeInputInSeconds = projectTimer.value * 60;
 
-        if(projectTimer.value <= 0 || projectName.value === '' || isNaN(projectTimer.value)){
-            alert('parametros incorretos');
+        if (
+            projectTimer.value <= 0 ||
+            projectName.value === "" ||
+            isNaN(projectTimer.value)
+        ) {
+            alert("parametros incorretos");
             return;
         }
+
+        salvarDadosCronometro('Em andamento')
 
         startButton.style.backgroundColor = "var(--cor-vermelha)";
         iconButton.src = "./assets/interromper-cronometro.svg";
@@ -38,13 +43,17 @@ document.addEventListener("DOMContentLoaded", () => {
                 updateTimerDisplay(totalSeconds);
 
                 if (totalSeconds >= timeInputInSeconds) {
-                    alert(`O tempo de ${projectTimer.value} minuto(s) foi atingido!`);
+                    alert(
+                        salvarDadosCronometro('finalizado')
+                        `O tempo de ${projectTimer.value} minuto(s) foi atingido!`
+                    );
                     interromperCronometro();
                 }
                 salvarEstadoCronometro();
             }, 1000);
         } else {
             pausarCronometro();
+            salvarDadosCronometro('interrompido');
             interromperCronometro();
         }
     };
@@ -56,40 +65,49 @@ document.addEventListener("DOMContentLoaded", () => {
             totalSeconds: totalSeconds,
             projectTimerInSeconds: timeInputInSeconds,
             projectTimerInminutes: projectTimer.value,
-            isRunning: isRunning
+            isRunning: isRunning,
         };
-        localStorage.setItem("cronometroState", JSON.stringify(cronometroState));
+        localStorage.setItem(
+            "cronometroState",
+            JSON.stringify(cronometroState)
+        );
     };
 
     const recuperarEstadoCronometro = () => {
-       
         const savedState = JSON.parse(localStorage.getItem("cronometroState"));
         if (savedState) {
             projectName.value = savedState.projectName;
             totalSeconds = savedState.totalSeconds;
             isRunning = savedState.isRunning;
             projectTimer.value = savedState.projectTimerInminutes;
-            const timeInputInSeconds = savedState.projectTimerInSeconds
+            const timeInputInSeconds = savedState.projectTimerInSeconds;
 
-            if(projectTimer.value <= 0 || projectName.value === '' || isNaN(projectTimer.value)){
-                alert('parametros incorretos');
+            if (
+                projectTimer.value <= 0 ||
+                projectName.value === "" ||
+                isNaN(projectTimer.value)
+            ) {
+                alert("parametros incorretos");
                 return;
             }
-                   
-         startButton.style.backgroundColor = "var(--cor-vermelha)";
-         iconButton.src = "./assets/interromper-cronometro.svg";
-         textButton.innerText = "interromper";
-         iconTemp.src = "./assets/cronometro-ativado.svg"; 
+
+            startButton.style.backgroundColor = "var(--cor-vermelha)";
+            iconButton.src = "./assets/interromper-cronometro.svg";
+            textButton.innerText = "interromper";
+            iconTemp.src = "./assets/cronometro-ativado.svg";
             updateTimerDisplay(totalSeconds);
 
-            if (!isRunning) {
-                isRunning = true
+            isRunning = true;
+            if (isRunning) {
                 timerInterval = setInterval(() => {
                     totalSeconds++;
                     updateTimerDisplay(totalSeconds);
-        
+
                     if (totalSeconds >= timeInputInSeconds) {
-                        alert(`O tempo de ${projectTimer.value} minuto(s) foi atingido!`);
+                        alert(
+                            `O tempo de ${projectTimer.value} minuto(s) foi atingido!`
+                        );
+                        salvarDadosCronometro('finalizado')
                         interromperCronometro();
                     }
                     salvarEstadoCronometro();
@@ -99,15 +117,13 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     const interromperCronometro = () => {
-       
-        salvarDadosCronometro();
         salvarEstadoCronometro();
         startButton.style.backgroundColor = "var(--cor-verde)";
         iconButton.src = "./assets/iniciar-cronometro.svg";
         textButton.innerText = "Começar";
         iconTemp.src = "./assets/cronometro-desativado.svg";
-        projectName.value = ""
-        projectTimer.value = '0'
+        projectName.value = "";
+        projectTimer.value = "0";
 
         cards[0].textContent = "0";
         cards[1].textContent = "0";
@@ -120,20 +136,18 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     const pausarCronometro = () => {
-        
-        clearInterval(timerInterval)
-        timerInterval = 0
+        clearInterval(timerInterval);
+        timerInterval = 0;
         startButton.style.backgroundColor = "var(--cor-verde)";
         iconButton.src = "./assets/iniciar-cronometro.svg";
         textButton.innerText = "Começar";
         iconTemp.src = "./assets/cronometro-desativado.svg";
         isRunning = false;
-        }
-    
+    };
+
     const updateTimerDisplay = (totalSeconds) => {
         const minutes = Math.floor(totalSeconds / 60);
         const seconds = totalSeconds % 60;
-
         const minutesArray = String(minutes).padStart(2, "0").split("");
         const secondsArray = String(seconds).padStart(2, "0").split("");
 
@@ -143,24 +157,35 @@ document.addEventListener("DOMContentLoaded", () => {
         cards[3].textContent = secondsArray[1];
     };
 
-    const salvarDadosCronometro = () => {
+    const salvarDadosCronometro = (status) => {
+
         const cronometroData = {
             projectName: projectName.value,
-            duration: formatTime(totalSeconds),
-            startTime: new Date().toLocaleString()
+            duration: projectTimer.value,
+            startTime: new Date().toLocaleString(),
+            result: status
         };
 
-        const storedData = JSON.parse(localStorage.getItem("cronometroData")) || [];
+        const storedData =
+            JSON.parse(localStorage.getItem("cronometroData")) || [];
         storedData.push(cronometroData);
         localStorage.setItem("cronometroData", JSON.stringify(storedData));
     };
 
     const renderizarHistorico = () => {
-
-        const storedData = JSON.parse(localStorage.getItem("cronometroData")) || [];
+        const historicoContainer = document.getElementById("body-resultado");
+        const storedData =
+            JSON.parse(localStorage.getItem("cronometroData")) || [];
+        isRunning = true;
+        temporizador.classList.add("esconder");
+        tabelaGrid.classList.remove("esconder");
+        history.src = "./assets/historico-ativado.svg";
+        iconTemp.src = "./assets/cronometro-desativado.svg";
+        temporizador.classList.add("esconder");
+        tabelaGrid.classList.remove("esconder");
+        historicoContainer.innerHTML = "";
 
         storedData.forEach((data) => {
-            const historicoContainer = document.getElementById("body-resultado");
             historicoContainer.innerHTML += `
                 <div class=" body-tabela-resultados">
                     <div class="uk-child-width-1-3 uk-text-center uk-flex" uk-grid>
@@ -174,7 +199,7 @@ document.addEventListener("DOMContentLoaded", () => {
                             <div class="uk-card">${data.startTime}</div>
                         </div>
                         <div class="padding">
-                            <div class="uk-card">status</div>
+                            <div class="uk-card">${data.result}</div>
                         </div>
                     </div>
                 </div>
@@ -182,36 +207,17 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     };
 
-    const mudarParaTemporizador = () =>{  
-        temporizador.classList.remove('esconder');
-        tabelaGrid.classList.add('esconder');
+    const mudarParaTemporizador = () => {
+        temporizador.classList.remove("esconder");
+        tabelaGrid.classList.add("esconder");
         history.src = "./assets/historico.svg";
         iconTemp.src = "./assets/cronometro-ativado.svg";
-    }
-
-    const formatTime = (seconds) => {
-        const minutes = Math.floor(seconds / 60);
-        const remainingSeconds = seconds % 60;
-        return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
     };
-
-    startButton.addEventListener('click', iniciarOuPausarCronometro);
-    history.addEventListener('click', () =>{
-        isRunning = true;
-        temporizador.classList.add('esconder')
-        tabelaGrid.classList.remove('esconder')
-        history.src = "./assets/historico-ativado.svg";
-        iconTemp.src = "./assets/cronometro-desativado.svg";
-        temporizador.classList.add('esconder')
-        tabelaGrid.classList.remove('esconder')
-    });
-    ancorIconTemporizador.addEventListener('click', mudarParaTemporizador);
-    ancorIconLogo.addEventListener('click', recuperarEstadoCronometro)
- 
+    startButton.addEventListener("click", iniciarOuPausarCronometro);
+    history.addEventListener("click", renderizarHistorico);
+    ancorIconTemporizador.addEventListener("click", mudarParaTemporizador);
+    ancorIconLogo.addEventListener("click", recuperarEstadoCronometro);
 
     updateTimerDisplay(totalSeconds);
-    
-    if(tabelaGrid.classList.contains('esconder')){
-       window.onload = renderizarHistorico();
-    }
 });
+
